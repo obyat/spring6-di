3e9b25d0f61e180d2b77690af5fca9afee572347
Obyat <rahman.obyat@gmail.com>
2026-05-16 00:04:51 -0400

    Add qualifier annotations and new greeting service implementations

---
+++ src/main/java/guru/springframework/spring6di/services/GreetingServicePropertyInjected.java
@@ -0,0 +1,12 @@
+package guru.springframework.spring6di.services;
+
+
+import org.springframework.stereotype.Service;
+
+@Service("propertyGreetingService")
+public class GreetingServicePropertyInjected implements GreetingService {
+    @Override
+    public String sayGreeting() {
+        return "Property Injected";
+    }
+}
